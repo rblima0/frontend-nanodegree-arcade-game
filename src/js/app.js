9815c193624097135f8944356232933bc779cf8f
Rodrigo Banci <rblima0@gmail.com>
2018-03-15 11:23:36 -0300

    Resetando inimigo e velocidade quando chega no fim do cenario

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 // INIMIGO
-var Enemy = function(x, y, velocity) {
+var Enemy = function(x, y, originalY, velocity) {
     this.x = x;
     this.y = y;
+    this.originalY = originalY;
     this.velocity = velocity;
     this.sprite = 'images/enemy-bug.png';
     this.colid = false;
@@ -9,10 +10,21 @@ var Enemy = function(x, y, velocity) {
 
 Enemy.prototype.update = function(dt) {
     this.x += this.velocity * dt;
+    if (this.x >= 470) {
+        this.reset();
+    } 
 };
 
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.reset = function() {
+    if (this.x > 450) {
+        this.x = Math.random() * -80;
+        this.y = this.originalY;
+        this.velocity = Math.random() * (200 - 110) + 110;
+    }
 };
 
 // PLAYER
@@ -51,10 +63,10 @@ Player.prototype.collision = function(){
 
 var allEnemies = [];
 var player = new Player(200, 400);
-var enemyOne = new Enemy(5, 60, 70);
-var enemyTwo = new Enemy(5, 140, 30);
-var enemyThree = new Enemy(5, 220, 60);
-var enemyFour = new Enemy(5, 300, 40);
+var enemyOne = new Enemy(-80, 60, 60, 80);
+var enemyTwo = new Enemy(-80, 140, 140, 180);
+var enemyThree = new Enemy(-80, 220, 220, 60);
+var enemyFour = new Enemy(-80, 300, 300, 140);
 
 /* var enemyOne = new Enemy(5, 60, 50);
 var enemyTwo = new Enemy(105, 140, 50);
