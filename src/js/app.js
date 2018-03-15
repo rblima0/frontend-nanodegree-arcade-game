456a621e1b7625e1feb368091308ce54cb011044
Rodrigo Banci <rblima0@gmail.com>
2018-03-15 09:59:58 -0300

    Update inicial, cenario, inimigos e personagem aparecendo na tela

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,46 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// INIMIGO
+var Enemy = function(x, y) {
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// PLAYER
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+Player.prototype.handleInput = function(code) {
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+}
+
+var allEnemies = [];
+var player = new Player(200, 400);
+var enemyOne = new Enemy(5, 60);
+var enemyTwo = new Enemy(105, 140);
+var enemyThree = new Enemy(205, 220);
+var enemyFour = new Enemy(305, 300);
+
+allEnemies.push(enemyOne, enemyTwo, enemyThree, enemyFour);
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
